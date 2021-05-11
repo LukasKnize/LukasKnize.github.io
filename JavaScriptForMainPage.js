99944167387e9f0c5cd8e1a1dde88d6482dd0c50
LukasKnize <82168954+LukasKnize@users.noreply.github.com>
2021-05-11 14:34:41 +0200

    Add files via upload

--- JavaScriptForMainPage.js
+++ JavaScriptForMainPage.js
@@ -40,18 +40,25 @@ function openPortfolio(text) {
     document.getElementById("portfolioObjectBackground").style.display = "flex";
     if (text == "w1") {
         document.getElementById("portfolioTextH2").innerText = "Osobní webová stránka";
-        document.getElementById("portfolioTextP").innerText = "2Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quibusdam facilis sequi earum, qui reprehenderit cum provident quaerat delectus porro accusamus facere maiores libero ipsa quasi eveniet dicta doloremque autem?"
+        document.getElementById("portfolioTextP").innerText = "Moje osobní webová stránka vznikla jako závěrečná práce předmětu webové aplikace na Smíchovské střední průmyslové škole a gymnáziu.";
+        document.getElementById("ProjectLink").setAttribute("href", "https://www.LukasKnize.github.io");
+    }
+
+    if (text == "w2") {
+        document.getElementById("portfolioTextH2").innerText = "stránka pro školní projekt  ";
+        document.getElementById("portfolioTextP").innerText = "Tuto webovou stránku jsem vytvořil pro školní projekt z oblasti marketingu a propagace na internetu. Jedná se o stránku pro fiktivní firmu z oblasti hardwaru a elektroniky.";
+        document.getElementById("ProjectLink").setAttribute("href", "https://powershieldelectronics.github.io");
     }
 
     if (text == "g1") {
         document.getElementById("portfolioTextH2").innerText = "Grafické portfolio";
-        document.getElementById("portfolioTextP").innerText = "3Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quibusdam facilis sequi earum, qui reprehenderit cum provident quaerat delectus porro accusamus facere maiores libero ipsa quasi eveniet dicta doloremque autem?"
+        document.getElementById("portfolioTextP").innerText = "Na Behance pomalu buduji menší grafické portfolio, zatím zde toho moc nemám, ale dobudoucna snad budu přidávat další práce.";
         document.getElementById("ProjectLink").setAttribute("href", "https://www.behance.net/lukkne");
     }
 
     if (text == "h1") {
         document.getElementById("portfolioTextH2").innerText = "Počítačové hry";
-        document.getElementById("portfolioTextP").innerText = "4Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quibusdam facilis sequi earum, qui reprehenderit cum provident quaerat delectus porro accusamus facere maiores libero ipsa quasi eveniet dicta doloremque autem?"
+        document.getElementById("portfolioTextP").innerText = "Na itch.io mám několik menších her (spíše konceptů), které tvořím v enginu Unity. Většina vznikla během několika dnů v rámci Game jamů.";
         document.getElementById("ProjectLink").setAttribute("href", "https://tortoiseshell.itch.io");
     }
 }
